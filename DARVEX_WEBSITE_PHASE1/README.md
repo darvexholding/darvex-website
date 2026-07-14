74e919872e2929ea4eed44bc8517d51631c6f545
darvexholding <admin@darvexholding.com>
2026-07-14 22:02:17 +0300

    Add files via upload

---
+++ DARVEX_WEBSITE_PHASE1/README.md
@@ -0,0 +1,3 @@
+# DARVEX Website Phase 1
+
+Working static architecture for Cloudflare Pages.
---
+++ DARVEX_WEBSITE_PHASE1/js/app.js
@@ -0,0 +1,2 @@
+const b=document.querySelector('.menu-toggle'),n=document.querySelector('.main-nav');
+if(b&&n)b.addEventListener('click',()=>n.classList.toggle('open'));
